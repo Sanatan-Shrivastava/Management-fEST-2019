5abf2485b8c46ed8680906c205127403f48b9303
DMS-Fest <sanatan.dmsfest@gmail.com>
2019-10-18 16:13:55 +0530

    signin testing

--- register.js
+++ register.js
@@ -11,11 +11,3 @@ function onSignIn(googleUser) {
 
 
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-    console.log('User signed out.');
-    });
-    $(".g-signin2").css("display","block");
-    $(".data").css("display","none");
-}
