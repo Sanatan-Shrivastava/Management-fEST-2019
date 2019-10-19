6e324c4d39edc28312d556ab3a2938f2e8cd7643
Sanatan-Shrivastava <sanatan.cseiiitk@gmail.com>
2019-10-19 20:26:45 +0530

    added clientID

--- firebase.js
+++ firebase.js
@@ -7,6 +7,7 @@
     storageBucket: "dms-fest.appspot.com",
     messagingSenderId: "563054154548",
     appId: "1:563054154548:web:5893d4e9c896030335cb61",
-    measurementId: "G-5SVRGZDFDL"
+    measurementId: "G-5SVRGZDFDL",
+    clientId: "563054154548-1052l9kng0thf5haspm9g425togsqfld.apps.googleusercontent.com"
   };
   firebase.initializeApp(config);
