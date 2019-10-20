23ed6c07f70eb45a63fa3ad6376bb04e01403d6a
Shaktiraj Daudra <shaktirajdaudra@gmail.com>
2019-10-20 23:30:23 +0530

    bug fixes

--- enteries.js
+++ enteries.js
@@ -1,57 +1,61 @@
 function setData() {
     try{
         //Initialize Elements
-    var db = firebase.database();
-    var mName = document.getElementById("name");
-    var mCollege = document.getElementById("college");
-    var mNumber = document.getElementById("number");
-    var mMnitians = document.getElementById("mnitians");
+    // var db = firebase.database();
+    // var mName = document.getElementById("name");
+    // var mCollege = document.getElementById("college");
+    // var mNumber = document.getElementById("number");
+    // var mMnitians = document.getElementById("mnitians");
 
-    //Store value in string
-    var name = mName.value;
-    var college = mCollege.value;
-    var number = mNumber.value;
-    var mnitians = mMnitians.value;
+    // //Store value in string
+    // var name = mName.value;
+    // var college = mCollege.value;
+    // var number = mNumber.value;
+    // var mnitians = mMnitians.value;
 
-    //Some hardcoded data
-    //Todo-
-    //1) Add real festid
-    var festid = "fest@1234"
+    // //Some hardcoded data
+    // //Todo-
+    // //1) Add real festid
+    // var festid = "fest@1234"
     
-    // var uniqueId = "uniqueId";
-    var uniqueId = "waiting for id";
+    // // var uniqueId = "uniqueId";
+    // var uniqueId = "waiting for id";
+    
+    var user = firebase.auth().currentUser;
+        window.alert(user.uid);
 
-    gapi.load('auth2', function() {
-        auth2 = gapi.auth2.init({
-          client_id: '563054154548-u037bg5kvu45gegtoofmtr0c6ioql1ft.apps.googleusercontent.com',
-          fetch_basic_profile: false,
-          scope: 'profile'
-        });
-
-        if (auth2.isSignedIn.get()) {
-            var profile = auth2.currentUser.get().getBasicProfile();
-            console.log('ID: ' + profile.getId());
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-            uniqueId = profile.getId();
-            var userDetails = {
-                name: name,
-                college: college,
-                phone: number,
-                mnitians: mnitians,
-                festid: festid
-            };
+    // uniqueId = profile.getId();
+    //         var userDetails = {
+    //             name: name,
+    //             college: college,
+    //             phone: number,
+    //             mnitians: mnitians,
+    //             festid: festid
+    //         };
         
-            db.ref('users/' + uniqueId).set(userDetails);
-            location.href = './register2.html';
-        }else{
-            window.alert("not logged in");
-        }
+    // db.ref('users/' + uniqueId).set(userDetails);
+    // gapi.load('auth2', function() {
+    //     auth2 = gapi.auth2.init({
+    //       client_id: '563054154548-u037bg5kvu45gegtoofmtr0c6ioql1ft.apps.googleusercontent.com',
+    //       fetch_basic_profile: false,
+    //       scope: 'profile'
+    //     });
 
-    });
+    //     if (auth2.isSignedIn.get()) {
+    //         var profile = auth2.currentUser.get().getBasicProfile();
+    //         console.log('ID: ' + profile.getId());
+    //         console.log('Full Name: ' + profile.getName());
+    //         console.log('Given Name: ' + profile.getGivenName());
+    //         console.log('Family Name: ' + profile.getFamilyName());
+    //         console.log('Image URL: ' + profile.getImageUrl());
+    //         console.log('Email: ' + profile.getEmail());
+            
+    //         // location.href = './register2.html';
+    //     }else{
+    //         window.alert("not logged in");
+    //     }
+
+    // });
     }catch(error){
         // Handle Errors here.
         // var errorCode = error.code;
